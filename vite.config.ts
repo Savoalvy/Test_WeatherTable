ed5b41dce4084cdcd2ee22fe84e8133a406c6ff0
Savoalvy <Savoalvy2312@mail.ru>
2026-07-03 22:00:02 +0300

    Fix github view

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Test_WeatherTable/',
   plugins: [react()],
   resolve: {
     alias: {
